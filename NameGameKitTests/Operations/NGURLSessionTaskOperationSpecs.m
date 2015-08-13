f425df3799fe7ba29661b837f883662bb38716ec
Nate Armstrong <natearmstrong2@gmail.com>
2015-08-13 09:16:07 -0600

    Test that URLSessionTaskOperation calls `finish`

--- NameGameKitTests/Operations/NGURLSessionTaskOperationSpecs.m
+++ NameGameKitTests/Operations/NGURLSessionTaskOperationSpecs.m
@@ -9,15 +9,17 @@
 @import Foundation;
 @import Specta;
 @import Expecta;
+#import "OCMock.h"
 
 #import "NGURLSessionTaskOperation.h"
 
 SpecBegin(NGURLSessionTaskOperationSpec)
 
 describe(@"NGURLSessionTaskOperation", ^{
-	it(@"should complete task", ^{
+	it(@"should complete task and finish operation", ^{
 		__block NSURLSessionTask *task;
 		__block NGURLSessionTaskOperation *taskOperation;
+		__block id taskOperationMock;
 		waitUntil(^(DoneCallback done) {
         	NSOperationQueue *queue = [NSOperationQueue new];
         	NSURL *url = [NSURL URLWithString:@"http://api.namegame.willowtreemobile.com/"];
@@ -26,10 +28,12 @@ describe(@"NGURLSessionTaskOperation", ^{
         	};
         	task = [[NSURLSession sharedSession] downloadTaskWithURL:url completionHandler:completionHandler];
 			taskOperation = [[NGURLSessionTaskOperation alloc] initWithTask:task];
+			taskOperationMock = [OCMockObject partialMockForObject:taskOperation];
 			[queue addOperation:taskOperation];
 		});
 		expect(task.state).to.equal(NSURLSessionTaskStateCompleted);
 		expect(taskOperation.finished).to.beTruthy();
+		[[taskOperationMock verify] finish];
 	});
 });
 
